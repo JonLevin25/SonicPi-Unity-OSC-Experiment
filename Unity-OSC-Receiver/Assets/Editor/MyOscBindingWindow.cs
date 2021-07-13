477c7ea355c916c9a32576c63f201755c1994695
Jonathan Levin <jon.levin25@gmail.com>
2021-07-13 14:45:58 +0300

    OSC Window helper

---
+++ Unity-OSC-Receiver/Assets/Editor/MyOscBindingWindow.cs
@@ -0,0 +1,74 @@
+﻿using extOSC.Editor;
+using extOSC.Editor.Windows;
+using UnityEditor;
+using UnityEngine;
+
+namespace MyOscComponents.EditorNS
+{
+	
+	public class MyOscBindingWindow : OSCWindow<MyOscBindingWindow, OscBindingsPanel>
+	{
+		[MenuItem("Tools/My OSC/" + "OSC Bindings")]
+		public static void Open()
+		{
+			Instance.titleContent = new GUIContent("OSC Bindings", OSCEditorTextures.IronWallSmall);
+			Instance.minSize = new Vector2(550, 200);
+			Instance.Show();
+			Instance.Focus();
+		}
+		private readonly string _lastFileSettings = OSCEditorSettings.Debug + "lastfile";
+		
+		private OscBindingsPanel bindingsPanel;
+		
+		protected override void OnEnable()
+		{
+			bindingsPanel = new OscBindingsPanel(this);
+			base.OnEnable();
+		}
+
+		protected void OnInspectorUpdate()
+		{
+			bindingsPanel?.Refresh();
+			Repaint();
+		}
+		
+		protected override void SaveWindowSettings()
+		{
+			// if (bindingsPanel == null) return;
+			//
+			// var debugPacket = bindingsPanel.CurrentPacket;
+			// if (debugPacket != null)
+			// {
+			// 	if (string.IsNullOrEmpty(_packetEditorPanel.FilePath))
+			// 	{
+			// 		_packetEditorPanel.FilePath = OSCEditorUtils.BackupFolder + "unsaved.eod";
+			// 	}
+			//
+			// 	OSCEditorUtils.SavePacket(_packetEditorPanel.FilePath, debugPacket);
+			// 	OSCEditorSettings.SetString(_lastFileSettings, _packetEditorPanel.FilePath);
+			//
+			// 	return;
+			// }
+			//
+			// OSCEditorSettings.SetString(_lastFileSettings, "");
+		}
+
+		protected override void LoadWindowSettings()
+		{
+			// if (_packetEditorPanel == null) return;
+			//
+			// var lastOpenedFile = OSCEditorSettings.GetString(_lastFileSettings, "");
+			//
+			// if (!string.IsNullOrEmpty(lastOpenedFile))
+			// {
+			// 	var debugPacket = OSCEditorUtils.LoadPacket(lastOpenedFile);
+			// 	if (debugPacket != null)
+			// 	{
+			// 		_packetEditorPanel.CurrentPacket = debugPacket;
+			// 		_packetEditorPanel.FilePath = lastOpenedFile;
+			// 	}
+			// }
+		}
+
+	}
+}
---
+++ Unity-OSC-Receiver/Assets/Editor/OscBindingsPanel.cs
@@ -0,0 +1,208 @@
+﻿using System.Reflection;
+using extOSC;
+using extOSC.Core;
+using extOSC.Editor;
+using extOSC.Editor.Panels;
+using extOSC.Editor.Windows;
+using GDX;
+using UnityEditor;
+using UnityEngine;
+
+namespace MyOscComponents.EditorNS
+{
+    public class OscBindingsPanel : OSCPanel
+    {
+        private static readonly GUIContent _receiversContent = new GUIContent("Receivers:");
+        private static readonly GUIContent _triggeredFXContent = new GUIContent("Triggerable FX:");
+
+        private OscReceiverBinding[] _receiverBindings;
+        private Vector2 _scrollPosition;
+        private MethodInfo _receiveMethod;
+        private Color _defaultColor;
+        private OscTriggeredEffect[] _triggerableFX;
+
+        protected override void DrawContent(ref Rect contentRect)
+        {
+            _defaultColor = GUI.color;
+            using (new GUILayout.HorizontalScope(EditorStyles.toolbar))
+            {
+                DrawRefreshButton();
+                GUILayout.FlexibleSpace();
+            }
+
+            using (var scroll = new GUILayout.ScrollViewScope(_scrollPosition))
+            {
+                var expand = contentRect.width > 350;
+                if (expand) GUILayout.BeginHorizontal();
+
+                DrawReceivers();
+                DrawTriggeredFX();
+
+                if (expand) GUILayout.EndHorizontal();
+
+                _scrollPosition = scroll.scrollPosition;
+            }
+        }
+
+        private void DrawReceivers()
+        {
+            using (new GUILayout.VerticalScope())
+            {
+                GUILayout.Label(_receiversContent, OSCEditorStyles.ConsoleBoldLabel);
+
+                if (_receiverBindings.Length > 0)
+                {
+                    foreach (var t in _receiverBindings)
+                    {
+                        DrawTransBinding(t);
+                    }
+                }
+                else
+                {
+                    EditorGUILayout.HelpBox("Scene doesn't have any Receiver Bindings.", MessageType.Info);
+                }
+            }
+        }
+        
+        private void DrawTriggeredFX()
+        {
+            using (new GUILayout.VerticalScope())
+            {
+                GUILayout.Label(_triggeredFXContent, OSCEditorStyles.ConsoleBoldLabel);
+
+                if (_triggerableFX.Length > 0)
+                {
+                    foreach (var t in _triggerableFX)
+                    {
+                        DrawTriggerable(t);
+                    }
+                }
+                else
+                {
+                    EditorGUILayout.HelpBox("Scene doesn't have any Triggered FX.", MessageType.Info);
+                }
+            }
+        }
+
+        private void DrawRefreshButton()
+        {
+            // const float size = 40f;
+
+            // var oldSize = GUI.skin.button.fontSize;
+            // GUI.skin.button.fontSize = Mathf.RoundToInt(size * 0.5f);
+            // if (GUILayout.Button("↻", GUILayout.Width(size), GUILayout.Height(size)))
+            if (GUILayout.Button("↻"))
+            {
+                Refresh();
+            }
+
+            // GUI.skin.button.fontSize = oldSize;
+        }
+
+        public OscBindingsPanel(OSCWindow window) : base(window)
+        {
+            Refresh();
+        }
+
+        public void Refresh()
+        {
+            _receiverBindings = Object.FindObjectsOfType<OscReceiverBinding>();
+            _triggerableFX = Object.FindObjectsOfType<OscTriggeredEffect>();
+        }
+
+        private void DrawTransBinding(OscReceiverBinding transBind)
+        {
+            // GUI.color = osc.IsStarted ? Color.green : Color.red;
+            using (new GUILayout.VerticalScope(EditorStyles.helpBox))
+            {
+                using (new GUILayout.VerticalScope(OSCEditorStyles.Box))
+                {
+                    if (GUILayout.Button($"{transBind.name} ({transBind.transform.GetScenePath()})"))
+                    {
+                        EditorGUIUtility.PingObject(transBind);
+                        Selection.activeObject = transBind;
+                    }
+                    GUILayout.Label($"Address: {transBind.Address}");
+                }
+
+                // GUILayout.Label(_actionsContent);
+                using (new GUILayout.HorizontalScope(OSCEditorStyles.Box))
+                {
+                    // DrawActions(transBind);
+                }
+            }
+
+            GUI.color = _defaultColor;
+        }
+        
+        private void DrawTriggerable(OscTriggeredEffect triggerablke)
+        {
+            // GUI.color = osc.IsStarted ? Color.green : Color.red;
+            using (new GUILayout.VerticalScope(EditorStyles.helpBox))
+            {
+                using (new GUILayout.VerticalScope(OSCEditorStyles.Box))
+                {
+                    if (GUILayout.Button($"{triggerablke.name} ({triggerablke.transform.GetScenePath()})"))
+                    {
+                        EditorGUIUtility.PingObject(triggerablke);
+                        Selection.activeObject = triggerablke;
+                    }
+                }
+
+                // GUILayout.Label(_actionsContent);
+                using (new GUILayout.HorizontalScope(OSCEditorStyles.Box))
+                {
+                    // DrawActions(transBind);
+                }
+            }
+
+            GUI.color = _defaultColor;
+        }
+
+        private void DrawActions(OSCBase osc)
+        {
+            GUI.color = Color.yellow;
+            GUI.enabled = osc.IsStarted;
+
+            switch (osc)
+            {
+                case OSCTransmitter transmitter:
+                    if (GUILayout.Button("_sendActionContent"))
+                    {
+                        var debugPacket = OSCWindowDebug.CurrentPacket;
+                        if (debugPacket != null)
+                        {
+                            transmitter.Send(debugPacket.Copy(), OSCSendOptions.IgnoreBundle);
+                        }
+                    }
+
+                    break;
+
+                case OSCReceiver receiver:
+                    if (GUILayout.Button("_receiveActionContent"))
+                    {
+                        var debugPacket = OSCWindowDebug.CurrentPacket;
+                        if (debugPacket != null)
+                        {
+                            if (_receiveMethod == null)
+                                _receiveMethod = typeof(OSCReceiver).GetMethod("PacketReceived",
+                                    BindingFlags.Instance | BindingFlags.NonPublic);
+
+                            _receiveMethod.Invoke(receiver, new object[] {debugPacket.Copy()});
+                        }
+                    }
+
+                    break;
+            }
+
+            GUI.enabled = true;
+            GUI.color = _defaultColor;
+            
+            if (GUILayout.Button("_selectActionContent", GUILayout.MaxWidth(60)))
+            {
+                // EditorGUIUtility.PingObject(osc);
+                Selection.activeGameObject = osc.gameObject;
+            }
+        }
+    }
+}
