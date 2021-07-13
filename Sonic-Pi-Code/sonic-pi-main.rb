a5d7dc6805d51a101e1ff5fa4832d483490abf71
Jonathan Levin <jon.levin25@gmail.com>
2021-07-13 14:46:39 +0300

    sonic pi: TEMP

--- Sonic-Pi-Code/sonic-pi-main.rb
+++ Sonic-Pi-Code/sonic-pi-main.rb
@@ -1,7 +1,39 @@
 # Welcome to Sonic Pi
-use_osc "localhost", 4561
+use_real_time
+
+use_osc "localhost", 4561 # sender only
+def fx
+  val = get[:x]
+  with_fx :bitcrusher, mix: val do
+    yield
+  end
+end
+
 live_loop :test_osc do
-  sample :bd_haus
-  osc "/sonic/bd", 1.25
-  sleep 1
+  fx {
+    
+  }
+end
+
+live_loop :test_osc do
+  v = 0
+    4.times do
+  fx {
+      sample :drum_cymbal_closed
+      set :x, v
+      osc "/sonic/bd", 1.25
+      sleep 1
+      puts v
+  }
+  v += 0.25
+    end
+end
+
+live_loop :wait do
+  fx {
+    puts "x"
+  sync "/osc*/woot"
+  puts "y"
+  sample :drum_cymbal_closed
+  }
 end
