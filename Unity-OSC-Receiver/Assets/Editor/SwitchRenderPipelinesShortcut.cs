c4994aa9253a232c77de36ec7292787b0a960c28
Jonathan Levin <jon.levin25@gmail.com>
2021-07-14 18:06:33 +0300

    auto stuff & nature pack

---
+++ Unity-OSC-Receiver/Assets/Editor/SwitchRenderPipelinesShortcut.cs
@@ -0,0 +1,52 @@
+﻿using System.Linq;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace MyOscComponents.EditorNS
+{
+    public static class SwitchRenderPipelinesShortcut
+    {
+        [MenuItem("Tools/Toggle URP %U")]
+        public static void SwitchPipeline()
+        {
+            var pipelineAsset = GraphicsSettings.defaultRenderPipeline;
+            var wasOnSrp = pipelineAsset != null;
+            if (wasOnSrp)
+            {
+                GraphicsSettings.defaultRenderPipeline = null;
+                NotifyScene("Switched to (global) Default Renderer");
+            }
+            else
+            {
+                var pipelineAssets = AssetDatabase
+                    .FindAssets($"t: {typeof(RenderPipelineAsset).FullName}")
+                    .Select(AssetDatabase.GUIDToAssetPath)
+                    .Select(AssetDatabase.LoadAssetAtPath<RenderPipelineAsset>)
+                    .ToArray();
+
+                if (pipelineAssets.Length == 0)
+                {
+                    NotifyScene($"No pipelines found.");
+                    return;
+                }
+                
+                var pipe = pipelineAssets[0];
+                GraphicsSettings.defaultRenderPipeline = pipe;
+                if (pipelineAssets.Length > 1)
+                {
+                    NotifyScene($"Multiple pipelines. Set (global) to {pipe.name}");
+                }
+                else
+                {
+                    NotifyScene($"Set (global) pipeline to {pipe.name}");
+                }
+            }
+        }
+
+        private static void NotifyScene(string text)
+        {
+            SceneView.lastActiveSceneView.ShowNotification(new GUIContent(text));
+        }
+    }
+}
